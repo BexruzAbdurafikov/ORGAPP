e54d3f4abaa56cd243b17b9d41cc1364a0cf9e35
BexruzAbdurafikov <abdurafikovbexruz123@gmail.com>
2025-06-14 12:31:40 +0500

    edited

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    esbuild: {
+        target: 'esnext'
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                Projects: resolve(__dirname, 'src/pages/Projects.html'),
+                signin: resolve(__dirname, 'src/pages/signin.html'),
+                signup: resolve(__dirname, 'src/pages/signup.html'),
+                404: resolve(__dirname, 'src/pages/404.html'),
+                ProjectPage: resolve(__dirname, 'src/pages/ProjectPage.html'),
+            },
+        },
+    },
+})
