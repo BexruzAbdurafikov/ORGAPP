7582be81494560eef0d3d4ec888c32f8d2c080f4
BexruzAbdurafikov <abdurafikovbexruz123@gmail.com>
2025-06-14 12:16:26 +0500

    edited

--- frontend/src/scripts/signin.js
+++ frontend/src/scripts/signin.js
@@ -8,44 +8,40 @@ const loader = document.querySelector('#loader-overlay');
 
 loader.classList.add('hidden');
 
-window.addEventListener('DOMContentLoaded', () => {
+form.onsubmit = async (e) => {
+    e.preventDefault();
 
+    const user = {};
+    const fm = new FormData(form);
 
-    form.onsubmit = async (e) => {
-        e.preventDefault();
+    fm.forEach((value, key) => {
+        user[key] = value;
+    })
 
-        const user = {};
-        const fm = new FormData(form);
-
-        fm.forEach((value, key) => {
-            user[key] = value;
-        })
-
-        try {
-            const res = await axios.post(import.meta.env.VITE_API_URL + '/authentication', {
-                ...user,
-                strategy: 'local'
-            });
-            cookie.setCookie('accessToken', res.data.accessToken, 1);
-            useToast('success', 'Вы успешно вошли!');
-            setTimeout(() => {
-                window.location.href = 'Projects';
-            }, 2000);
-        } catch (e) {
-            const code = e.response.data?.code || e.response.status;
-            if (code === 400) {
-                useToast('error', 'Неверные данные. Проверьте форму.');
-            } else if (code === 401) {
-                useToast('error', 'Неверный логин или пароль.');
-            } else if (code === 403) {
-                useToast('error', 'Доступ запрещён.');
-            } else if (code === 404) {
-                useToast('error', 'Маршрут не найден.');
-            } else {
-                useToast('error', 'Ошибка входа: ' + code);
-            }
-        } finally {
-            loader.classList.add('hidden');
+    try {
+        const res = await axios.post(import.meta.env.VITE_API_URL + '/authentication', {
+            ...user,
+            strategy: 'local'
+        });
+        cookie.setCookie('accessToken', res.data.accessToken, 1);
+        useToast('success', 'Вы успешно вошли!');
+        setTimeout(() => {
+            window.location.href = 'Projects';
+        }, 2000);
+    } catch (e) {
+        const code = e.response.data?.code || e.response.status;
+        if (code === 400) {
+            useToast('error', 'Неверные данные. Проверьте форму.');
+        } else if (code === 401) {
+            useToast('error', 'Неверный логин или пароль.');
+        } else if (code === 403) {
+            useToast('error', 'Доступ запрещён.');
+        } else if (code === 404) {
+            useToast('error', 'Маршрут не найден.');
+        } else {
+            useToast('error', 'Ошибка входа: ' + code);
         }
-    };
-})
+    } finally {
+        loader.classList.add('hidden');
+    }
+};
