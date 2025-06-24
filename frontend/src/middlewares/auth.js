3a3a6db358e833e5158ec2bf6e95d7d099902df2
BexruzAbdurafikov <abdurafikovbexruz123@gmail.com>
2025-06-24 13:46:56 +0500

    edited

--- frontend/src/middlewares/auth.js
+++ frontend/src/middlewares/auth.js
@@ -1,26 +1,10 @@
-import { cookie } from "../utils/cookie.js";
-import { redirect } from "../utils/helpers.js";
-import { drawHeader } from "../components/Header.js";
+import { cookie } from "../utils/cookie"
+import { redirect } from "../utils/helpers";
 
 export default async () => {
-    const accessToken = cookie.getCookie('accessToken');
+    const acccessToken = cookie.getCookie('accessToken');
 
-    let payload = null;
-
-    if (accessToken && accessToken !== 'undefined') {
-        try {
-            payload = JSON.parse(atob(accessToken.split('.')[1]));
-        } catch (err) {
-            console.warn("Невалидный токен");
-        }
-    }
-
-    await drawHeader(payload);
-
-    const publicPaths = ['/signin', '/signup'];
-    const path = window.location.pathname;
-
-    if (!payload && !publicPaths.includes(path)) {
+    if (acccessToken === 'undefined' || !acccessToken) {
         return redirect('/signin');
     }
-};
+}
